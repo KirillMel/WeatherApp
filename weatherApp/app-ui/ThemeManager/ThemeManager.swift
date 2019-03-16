0e21e4f4dfa96a68d596b61e7d4fee8e5d7f533e
Kirill <kirill.melnichenkoo@gmail.com>
2019-03-16 10:46:41 +0200

    Save and retrieving theme from user defaults.

--- weatherApp/app-ui/ThemeManager/ThemeManager.swift
+++ weatherApp/app-ui/ThemeManager/ThemeManager.swift
@@ -8,6 +8,28 @@
 
 import Foundation
 
+enum Theme: String {
+    case light = "Light"
+    case dark = "Dark"
+}
+
 class ThemeManager {
-    static var current = 0
+    static var current: ThemeProtocol = LightTheme()
+    
+    static func getPreviousTheme() {
+        let theme = Theme(rawValue: (UserDefaults.standard.string(forKey: "ColorTheme")) ?? "Light")!
+        
+        switch theme {
+        case .light:
+            current = LightTheme()
+        case .dark:
+            current = DarkTheme()
+        default:
+            current = LightTheme()
+        }
+    }
+    
+    static func saveTheme(_ theme: Theme) {
+        UserDefaults.standard.set(theme.rawValue, forKey: "ColorTheme")
+    }
 }
